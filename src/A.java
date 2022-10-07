7fbe698d189a982bdb8d886fee15f4ced27e5255
jay <jayaprakashnaik64@gmail.com>
2022-10-07 18:12:20 +0530

    return value back up

---
+++ src/A.java
@@ -0,0 +1,12 @@
+
+public class A {
+		A(int x, char y, boolean b){
+			System.out.println(10);
+			System.out.println(y);
+			System.out.println(b);
+}
+ public static void main(String[] args) {
+	A a1 =new A(100, 'a', true);
+	    
+}
+}
